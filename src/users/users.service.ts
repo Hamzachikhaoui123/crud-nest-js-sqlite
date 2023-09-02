ccaef1553e6e6ac42ce488d4c5f8917564a13491
HamzaChikhaoui123 <kalilchikaoui235@gmail.com>
2023-09-02 11:19:22 +0200

    crud-user

---
+++ src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepository:Repository<User>){}
+  create(createUserDto: CreateUserDto) {
+    const newUser=this.userRepository.create(createUserDto)
+
+    return this.userRepository.save(newUser);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOne({where:{id}});
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    const user=this.findOne(id)
+    
+    if(!user){
+      throw new HttpException('User not exist',HttpStatus.NOT_FOUND)
+    }
+    return this.userRepository.update(id,updateUserDto);
+  }
+
+ async remove(id: number) {
+    // const user  = await this.findOne(id)
+
+    // return this.userRepository.remove(user);
+    
+    return this.userRepository.delete(id)
+  }
+}
